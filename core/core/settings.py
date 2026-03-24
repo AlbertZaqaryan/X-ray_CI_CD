aecb8c1f5983fa23947f3d8a9c6d3db12947d73c
Your Name <itm.albert.zaqaryan@gmail.com>
2026-03-24 20:18:39 +0400

    version 2

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e2$^-hf-$5f=vl0=x_umx0l$k+%kocnum-u2nuxhk5g#02%=$1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
